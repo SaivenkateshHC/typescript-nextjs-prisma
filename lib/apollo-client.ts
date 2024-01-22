4219f2ab0b57ef760e391609b6cf935728dd7f97
Sai Venkatesh H <sai@deepsense>
2024-01-23 00:28:56 +0530

    added cors to versel.json

--- lib/apollo-client.ts
+++ lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://typescript-nextjs-prisma.vercel.app',
+  uri: 'https://typescript-nextjs-prisma.vercel.app/api/graphql',
   cache: new InMemoryCache(),
 })
 
